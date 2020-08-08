bc6a5c5e6633b1c49c3c1c6f6e6559a8a1f7491c
mynameispyo <54756611+mynameispyo@users.noreply.github.com>
2020-08-08 18:57:39 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -10,18 +10,21 @@ for (var i=0; i<args.length; ++i) {
 
 function loadMainPage(start,end){
   let title = $_GET["title"];
-  if(title !== undefined && title !== ""){
+  if(title !== undefined && title !== null && title !== ""){
     for(let i=start;i<end+1;i++){
-      var node = document.createElement("A");
+      var node = document.createElement("LI");
+      var linkTag = document.createElement("A");
       var textnode = document.createTextNode(i.toString()+" 화");   
-      node.setAttribute("href", `/view.html?title=${title}&epi=${i}`)     
-      node.appendChild(textnode);                   
+      linkTag.setAttribute("href", `/view.html?title=${title}&epi=${i}`)     
+      linkTag.appendChild(textnode);     
+      node.appendChild(linkTag);              
       document.getElementById("wt-tiltes").appendChild(node);
     }
   }else{
     window.location.href = "/";
   }
 }
+
 
 async function loadJson () {
   var result = true; 
@@ -30,9 +33,22 @@ async function loadJson () {
   )
     .then(response => response.json())
     .then(json => {
-      document.title = json.webtoon.korean[$_GET["title"]] + " 무료 보기";
+      let korean_name = json.webtoon.korean[$_GET["title"]];
+      document.title = korean_name + " 무료 보기";
+      document.getElementById("title").innerHTML = korean_name;
+      document.getElementById("title-img").src = `https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/${$_GET["title"]}/${$_GET["title"]}.jpg`;
       let epi = json.webtoon.epi[$_GET["title"]];
       loadMainPage(parseInt(epi.start), parseInt(epi.end));
     });
 }
 loadJson (); 
+
+function toBottom() {
+  document.body.scrollTop = document.body.scrollHeight;
+  document.documentElement.scrollTop = document.documentElement.scrollHeight;
+}
+
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
