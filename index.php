113ef7e666f3d42919fbb14c74edea79580ed8f8
mynameispyo <54756611+mynameispyo@users.noreply.github.com>
2020-08-08 00:22:28 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="/js/main.js"></script>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+    <header>
+      <h1>완결된 웹툰 전체 무료보기</h1>
+    </header>
+<div class="wt-container" id="wt-container">
+<!--
+  <div onclick="loadTo('menu.php?title=Sound_Of_Your_Heart')">
+    <img src="https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/Sound_Of_Your_Heart/Sound_Of_Your_Heart.jpg">
+    <p>마음의 소리</p>
+  </div>
+-->
+</div>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <link rel="stylesheet" href="/css/menu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="/js/menu.js"></script>
+</head>
+<body>
+  <div id="wt-tiltes">
+  </div>
+  <a href="/">Main</a>
+</body>
+</html>
