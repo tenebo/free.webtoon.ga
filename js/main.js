3b155ded55d53cdadb23ddcb82bd18d5e6b2e890
mynameispyo <54756611+mynameispyo@users.noreply.github.com>
2020-08-13 16:59:52 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ async function loadJson () {
         //let remove_underline = input.replace(/_/g, " ");
         var node = document.createElement("DIV");
         node.setAttribute("onclick", `loadTo('menu.html?title=${input}')`);
+        node.setAttribute("class", "wt");
         var imgtag = document.createElement("IMG");
         imgtag.setAttribute("src", `https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/${input}/${input}.jpg`); 
         node.appendChild(imgtag);         
@@ -25,3 +26,19 @@ async function loadJson () {
     });
 }
 loadJson (); 
+
+
+function searchWt() { 
+    let input = document.getElementById('searchBar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('wt'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="list-item";                  
+        } 
+    } 
+} 
