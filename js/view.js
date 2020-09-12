0aae53868f639b0448b10df7bd83b9a4141bd90a
mynameispyo <54756611+mynameispyo@users.noreply.github.com>
2020-09-12 12:58:59 -0400

    Update view.js

--- js/view.js
+++ js/view.js
@@ -14,11 +14,15 @@ function loadMainPage(){
   if(title === undefined || epi === undefined){
     window.location.href = "/";
   }else{
-    document.title = title + " " + epi + "화";
-    document.getElementById("iframes").src = `https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/${title}/${epi}/${epi}.html`;
-    document.getElementById("leftBtnLink").href = `/view.html?title=${title}&epi=${parseInt(epi) - 1}`;
-    document.getElementById("middleBtnLink").href = "/menu.html?title=" + title;
-    document.getElementById("rightBtnLink").href = `/view.html?title=${title}&epi=${parseInt(epi) + 1}`;
+    fetch("/datas.json")
+    .then(response => response.json())
+    .then(json => {
+      document.title = json.webtoon.korean[title] + " " + epi + "화";
+      document.getElementById("iframes").src = `https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/${title}/${epi}/${epi}.html`;
+      document.getElementById("leftBtnLink").href = `/view.html?title=${title}&epi=${parseInt(epi) - 1}`;
+      document.getElementById("middleBtnLink").href = "/menu.html?title=" + title;
+      document.getElementById("rightBtnLink").href = `/view.html?title=${title}&epi=${parseInt(epi) + 1}`;
+    });
   }
 }
 
