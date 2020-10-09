98d9b8166f748cd49114a8085e8df6d3c4b39b31
mynameispyo <54756611+mynameispyo@users.noreply.github.com>
2020-10-09 10:29:36 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,47 +1,68 @@
-var $_GET = {},
-    args = location.search.substr(1).split(/&/);
-for (var i=0; i<args.length; ++i) {
-    var tmp = args[i].split(/=/);
-    if (tmp[0] != "") {
-        $_GET[decodeURIComponent(tmp[0])] = decodeURIComponent(tmp.slice(1).join("").replace("+", " "));
+Vue.component('list-webtoon', {
+  props: ['num', 'title'],
+  data:function(){
+    return {
+      imgSrc: `https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/${this.name}/${this.name}.jpg`,
     }
-}
+  },
+  template: `
+  <li>
+    <a v-bind:href="'/view.html?title='+title+'&epi='+num">{{num}} 화</a>
+  </li>
+  `,
+})
 
 
-function loadMainPage(start,end){
-  let title = $_GET["title"];
-  if(title !== undefined && title !== null && title !== ""){
-    for(let i=start;i<end+1;i++){
-      var node = document.createElement("LI");
-      var linkTag = document.createElement("A");
-      var textnode = document.createTextNode(i.toString()+" 화");   
-      linkTag.setAttribute("href", `/view.html?title=${title}&epi=${i}`)     
-      linkTag.appendChild(textnode);     
-      node.appendChild(linkTag);              
-      document.getElementById("wt-tiltes").appendChild(node);
+var app = new Vue({
+  el: '#vue',
+  data: {
+    WebtoonData: "",
+    is_loading:true,
+    //img_url: `https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/${this._GET["title"]}/${this._GET["title"]}.jpg`
+  },
+  mounted () {
+    fetch('/datas.json')
+      .then(response => response.json())
+      .then(json => (this.WebtoonData = json))
+      .then(this.load_title)
+      .finally(() => this.is_loading = false)
+  },
+  computed:{
+    _GET: function(){
+      let _GET = {};
+      let args= location.search.substr(1).split(/&/);
+      for (var i=0; i<args.length; ++i) {
+          var tmp = args[i].split(/=/);
+          if (tmp[0] != "") {
+              _GET[decodeURIComponent(tmp[0])] = decodeURIComponent(tmp.slice(1).join("").replace("+", " "));
+          }
+      }
+      if (this.WebtoonData.webtoon.titles.indexOf(_GET.title) === -1){
+        window.location.href = "/404.html"
+      }
+      return _GET;
+    },
+    korean_title_name: function(){
+      return this.WebtoonData.webtoon.korean[this._GET["title"]];
+    },
+    img_url: function(){
+      return 'https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/'+this._GET.title+'/'+this._GET.title+'.jpg';
+    },
+    range: function() {
+      let epi = this.WebtoonData.webtoon.epi[this._GET["title"]];
+      var ans = [];
+      for (let i = epi.start; i <= epi.end; i++) {
+          ans.push(i);
+      }
+      return ans;
+    },
+  },
+  methods: {
+    load_title: function(){
+      document.title = this.WebtoonData.webtoon.korean[this._GET.title] + " 무료 보기"
     }
-  }else{
-    window.location.href = "/";
-  }
-}
-
-
-async function loadJson () {
-  var result = true; 
-  await fetch(
-    "/datas.json"
-  )
-    .then(response => response.json())
-    .then(json => {
-      let korean_name = json.webtoon.korean[$_GET["title"]];
-      document.title = korean_name + " 무료 보기";
-      document.getElementById("title").innerHTML = korean_name;
-      document.getElementById("title-img").src = `https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/${$_GET["title"]}/${$_GET["title"]}.jpg`;
-      let epi = json.webtoon.epi[$_GET["title"]];
-      loadMainPage(parseInt(epi.start), parseInt(epi.end));
-    });
-}
-loadJson (); 
+  },
+})
 
 function toBottom() {
   document.body.scrollTop = document.body.scrollHeight;
