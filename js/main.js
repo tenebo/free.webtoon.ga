08efc65ef46aee1b64f3180d4a3cd254cd1e3b6b
mynameispyo <54756611+mynameispyo@users.noreply.github.com>
2020-10-09 10:22:26 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,43 @@
+Vue.component('webtoontitle', {
+  props: ['name', "koreanname"],
+  data:function(){
+    return {
+      imgSrc: `https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/${this.name}/${this.name}.jpg`,
+    }
+  },
+  methods:{
+    onclickurl:function(){
+      window.location.href = `/menu.html?title=${this.name}`
+    },
+  },
+  template: `
+  <div v-on:click="this.onclickurl" class="wt">
+    <img v-bind:src="imgSrc">
+    <p class="korean-font">{{koreanname}}</p>
+  </div>
+  `,
+})
+
+var app = new Vue({
+  el: '#vue',
+  data: {
+    WebtoonData: ""
+  },
+  beforeMount(){
+    this.getName()
+  },
+  methods: {
+    async getName(){
+      const res = await fetch('/datas.json');
+      const data = await res.json();
+      this.WebtoonData = data;
+    }
+  }
+})
+
 function loadTo(url){
   window.location.href = url;
 }
-
-async function loadJson () {
-  var result = true; 
-  await fetch(
-    "/datas.json"
-  )
-    .then(response => response.json())
-    .then(json => {
-      json.webtoon.titles.forEach(function(input){
-        //let remove_underline = input.replace(/_/g, " ");
-        var node = document.createElement("DIV");
-        node.setAttribute("onclick", `loadTo('menu.html?title=${input}')`);
-        node.setAttribute("class", "wt");
-        var imgtag = document.createElement("IMG");
-        imgtag.setAttribute("src", `https://qpo9w6tvvhkbvesizqvshw-on.drv.tw/web/${input}/${input}.jpg`); 
-        node.appendChild(imgtag);         
-        var name = document.createElement("P");
-        name.setAttribute("class", "korean-font")
-        name.appendChild(document.createTextNode(json.webtoon.korean[input]));
-        node.appendChild(name);
-        document.getElementById("wt-container").appendChild(node);
-      })
-    });
-}
-loadJson (); 
-
 
 function searchWt() { 
     let input = document.getElementById('searchBar').value 
